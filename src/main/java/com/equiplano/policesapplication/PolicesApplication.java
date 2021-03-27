ffa4a5910c2dfa5edfdeb99c35db56076164056b
Thiago Usetelni <thiago.usetelni.ext@e-ai.com.br>
2021-03-27 16:22:16 -0300

    initialize project

---
+++ src/main/java/com/equiplano/policesapplication/PolicesApplication.java
@@ -0,0 +1,13 @@
+package com.equiplano.policesapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equiplano/policesapplication/PolicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equiplano.policesapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
